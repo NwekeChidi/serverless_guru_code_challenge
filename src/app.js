4065515fa4ce919022e8145b41906776c7bf5eae
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-06-17 18:40:43 +0100

    add path /

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ app.use(express.json());
 // routes
 app.use("/users", require("./routes/users"));
 
+app.use("/", (req, res, next) => {
+    return res.sendStatus(200);
+  });
+
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
