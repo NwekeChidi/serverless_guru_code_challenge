9a1a941cbc18ddc05f2530ea48314ff73cd7e7d2
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-06-17 21:32:43 +0100

    feat: get blog post

---
+++ src/lambda/blogLambda.js
@@ -0,0 +1,40 @@
+const dynamo = require("../db/dynamo");
+
+const blogLambda = {}
+
+blogLambda.create = async (req, res) => {
+    const payload = req.body;
+    payload.blogger = req.params?.userId;
+    if ("post" in payload && typeof payload.post === "string"){
+        data = await dynamo.create(payload, "blog");
+        if ("error" in data){
+            return res.status(500).json({
+                    status: "failed",
+                    error: data.error
+                });
+        }
+        res.json({
+            status: "success",
+            data: data
+        });
+    }
+};
+
+
+blogLambda.getPost = async (req, res) => {
+    const { sortKey } = req.params;
+    const item = await dynamo.getBlogPost(sortKey);
+    if ("error" in item) {
+         return res.status(item.statusCode).json({
+                status: "failed",
+                error: item.error
+            });
+    }
+    res.json({
+        status: "success",
+        item
+    })
+
+}
+
+module.exports = blogLambda;
--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 const blogLambda = require("../lambda/blogLambda");
 
-// create user
+// create blog post
 router.post(
     "/new/:userId",
     blogLambda.create
 );
 
+// get blog post
+router.get(
+    "/view/:sortKey",
+    blogLambda.getPost
+);
 module.exports = router;
