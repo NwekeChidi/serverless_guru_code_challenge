c424a51907cbfcb0580d07f5aec0c1c375fb2200
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-06-18 11:23:18 +0100

    complete functions

--- src/lambda/blogLambda.js
+++ src/lambda/blogLambda.js
@@ -1,69 +1,71 @@
 const dynamo = require("../dbServices/dynamo");
 
-const blogLambda = {};
+const blogLambda = {}
 
 blogLambda.create = async (req, res) => {
-  const payload = req.body;
-  payload.blogger = req.params?.userId;
-  if ("post" in payload && typeof payload.post === "string") {
-    data = await dynamo.create(payload, "blog");
-    if ("error" in data) {
-      return res.status(500).json({
-        status: "failed",
-        error: data.error,
-      });
+    const payload = req.body;
+    payload.blogger = req.params?.userId;
+    if ("post" in payload && typeof payload.post === "string"){
+        data = await dynamo.create(payload, "blog");
+        if ("error" in data){
+            return res.status(500).json({
+                    status: "failed",
+                    error: data.error
+                });
+        }
+        res.json({
+            status: "success",
+            data: data
+        });
     }
-    res.json({
-      status: "success",
-      data: data,
-    });
-  }
 };
+
 
 blogLambda.getPost = async (req, res) => {
-  const { id } = req.params;
-  const item = await dynamo.getBlogPost(id);
-  if ("error" in item) {
-    return res.status(item.statusCode).json({
-      status: "failed",
-      error: item.error,
-    });
-  }
-  res.json({
-    status: "success",
-    item,
-  });
-};
+    const { id } = req.params;
+    const item = await dynamo.getBlogPost(id);
+    if ("error" in item) {
+         return res.status(item.statusCode).json({
+                status: "failed",
+                error: item.error
+            });
+    }
+    res.json({
+        status: "success",
+        item
+    })
+}
 
 blogLambda.updatePost = async (req, res) => {
-  const { id } = req.params;
-  const { body } = req;
-  const result = await dynamo.updatePost(id, body);
-  if ("error" in result) {
-    return res.status(result.statusCode).json({
-      status: "failed",
-      error: result.error,
-    });
-  }
-  res.json({
-    status: "success",
-    message: result,
-  });
-};
+    const { id } = req.params;
+    const { body } = req;
+    const result = await dynamo.updatePost(id, body);
+    if ("error" in result) {
+        return res.status(result.statusCode).json({
+            status: "failed",
+            error: result.error
+        });
+    }
+    res.json({
+        status: "success",
+        message: result
+    })
+}
+
 
 blogLambda.deletePost = async (req, res) => {
-  const { id } = req.params;
-  const result = await dynamo.deletePost(id);
-  if ("error" in result) {
-    return res.status(result.statusCode).json({
-      status: "failed",
-      error: result.error,
-    });
-  }
-  res.json({
-    status: "success",
-    message: result,
-  });
-};
+    const { id } = req.params;
+    const result = await dynamo.deletePost(id);
+    if ("error" in result) {
+        return res.status(result.statusCode).json({
+            status: "failed",
+            error: result.error
+        });
+    }
+    res.json({
+        status: "success",
+        message: result
+    })
+}
 
 module.exports = blogLambda;
